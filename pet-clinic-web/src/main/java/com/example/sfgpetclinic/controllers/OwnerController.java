3d0d20bc02f7f84dcd2de0381d59f179d126b94f
rajSon5512 <rjvsonawala@gmail.com>
2022-09-11 13:44:21 +0200

    owner index and controller page, closes #24

---
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package com.example.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+    
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listofowners(){
+        return "owners/index";
+    }
+}
